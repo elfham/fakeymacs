fa0db6cb95d4e6f9d2d73f2d4e1a9c110f81b014
elfham <38372058+elfham@users.noreply.github.com>
2021-01-09 15:13:03 +0900

    add DupFileEliminator

--- config_personal.py
+++ config_personal.py
@@ -55,6 +55,7 @@ fc.not_emacs_target += [
     "Minecraft.Windows.exe",        # Minecraft
     "ATrainPC.exe",                 # A-Train PC
     "hl2.exe",                      # Portal
+    "DupFileEliminator.exe",        # DupFileEliminator
 ]
 
 # IME の切り替え“のみをしたい”アプリケーションソフトを指定する
